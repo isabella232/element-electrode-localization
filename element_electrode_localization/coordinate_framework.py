7d54f317fe8fc19af8778b1b9f6ed755a528f35a
Thinh Nguyen <thinh@vathes.com>
2022-03-11 12:05:22 -0600

    helper for loading ccf data

--- element_electrode_localization/coordinate_framework.py
+++ element_electrode_localization/coordinate_framework.py
@@ -1,6 +1,13 @@
+import logging
+import pathlib
+
 import datajoint as dj
+import pandas as pd
+import numpy as np
+import nrrd
 
 
+log = logging.getLogger(__name__)
 schema = dj.schema()
 
 
@@ -17,7 +24,6 @@ def activate(schema_name, *, create_schema=True, create_tables=True):
 @schema
 class CCF(dj.Lookup):
     definition = """  # Common Coordinate Framework
-    # CCF Dataset Information
     ccf_id:             int             # CCF ID
     ---
     ccf_version:        int             # Allen CCF Version - e.g. CCFv3
@@ -50,7 +56,7 @@ class BrainRegionAnnotation(dj.Lookup):
         color_code=null: varchar(6)  # hexcode of the color code of this region
         """
 
-    class Voxel(dj.Manual):
+    class Voxel(dj.Part):
         definition = """
         -> master.BrainRegion
         -> CCF.Voxel
@@ -66,3 +72,91 @@ class ParentBrainRegion(dj.Lookup):
     """
 
 
+# ---- HELPERS ----
+
+
+def load_ccf_annotation(ccf_id, version_name, voxel_resolution,
+                        nrrd_filepath, ontology_csv_filepath, colorcode_csv_filepath):
+    """
+    Load the CCF r3 10 uM NRRD Dataset scaled to 20um.
+
+    Expects dj.config:
+
+        "custom": {
+            "ccf_data_paths": {
+                "version_name": "CCF_2017",
+                "annotation_nrrd": "annotation_10.nrrd",
+                "region_csv": "mousebrainontology_2.csv",
+                "hexcode_csv": "hexcode.csv"
+            }
+        }
+
+    see also:
+
+    http://download.alleninstitute.org/informatics-archive/current-release/mouse_ccf/annotation/ccf_2017
+
+    """
+    nrrd_filepath = pathlib.Path(nrrd_filepath)
+    ontology_csv_filepath = pathlib.Path(ontology_csv_filepath)
+    colorcode_csv_filepath = pathlib.Path(colorcode_csv_filepath)
+
+    regions = get_ontology_regions(ontology_csv_filepath, colorcode_csv_filepath)
+    stack, hdr = nrrd.read(nrrd_filepath)  # AP (x), DV (y), ML (z)
+
+    log.info('.. loaded atlas brain volume of shape {} from {}'
+             .format(stack.shape, nrrd_filepath))
+
+    ccf_key = {'ccf_id': ccf_id}
+    ccf_entry = {**ccf_key,
+                 'ccf_version': version_name,
+                 'ccf_description': f'Version: {version_name}'
+                                    f' - Voxel resolution (uM): {voxel_resolution}'
+                                    f' - Volume file: {nrrd_filepath.name}'
+                                    f' - Region ontology file: {ontology_csv_filepath.name},'
+                                    f' {colorcode_csv_filepath.name}'}
+
+    with dj.conn().transaction:
+        CCF.insert1(ccf_entry)
+        BrainRegionAnnotation.insert1(ccf_key)
+        BrainRegionAnnotation.BrainRegion.insert([
+            dict(ccf_entry,
+                 acronym=r.region_name,
+                 region_id=region_id,
+                 region_name=r.region_name,
+                 color_code=r.hexcode) for region_id, r in regions.iterrows()])
+
+        # Process voxels per brain region
+        for idx, (region_id, r) in enumerate(regions.iterrows()):
+            region_id = int(region_id)
+
+            log.info('.. loading region {} ({}/{}) ({})'
+                     .format(region_id, idx, len(regions), r.region_name))
+
+            # extracting filled volumes from stack in scaled [[x,y,z]] shape,
+            vol = (np.array(np.where(stack == region_id)).T * voxel_resolution)
+            vol = pd.DataFrame(vol, columns=['x', 'y', 'z'])
+
+            if not vol.shape[0]:
+                log.info('.. region {} volume: shape {} - skipping'
+                         .format(region_id, vol.shape))
+                continue
+            else:
+                log.info('.. region {} volume: shape {}'.format(
+                    region_id, vol.shape))
+
+            vol['ccf_id'] = [ccf_key['ccf_id']] * len(vol)
+            CCF.Voxel.insert(vol)
+
+            vol['acronym'] = [r.region_name] * len(vol)
+            BrainRegionAnnotation.Voxel.insert(vol)
+
+    log.info('.. done.')
+
+
+def get_ontology_regions(ontology_csv_filepath, colorcode_csv_filepath):
+    regions = pd.read_csv(ontology_csv_filepath, header=None, index_col=0)
+    regions.columns = ['region_name']
+    hexcode = pd.read_csv(colorcode_csv_filepath, header=None, index_col=0)
+    hexcode.columns = ['hexcode']
+
+    return pd.concat([regions, hexcode], axis=1)
