3f1df422982733ae69a3b8d26a8aded97a10e08b
Thinh Nguyen <thinh@vathes.com>
2022-03-24 18:43:01 -0500

    cleanup

--- element_electrode_localization/coordinate_framework.py
+++ element_electrode_localization/coordinate_framework.py
@@ -104,11 +104,10 @@ def load_ccf_annotation(ccf_id, version_name, voxel_resolution,
     nrrd_filepath = pathlib.Path(nrrd_filepath)
     ontology_csv_filepath = pathlib.Path(ontology_csv_filepath)
 
-    ontology = pd.read_csv(ontology_csv_filepath)
-
     def to_snake_case(s):
         return re.sub(r'(?<!^)(?=[A-Z])', '_', x).lower()
 
+    ontology = pd.read_csv(ontology_csv_filepath)
     ontology.acronym.apply(to_snake_case)
 
     stack, hdr = nrrd.read(nrrd_filepath.as_posix())  # AP (x), DV (y), ML (z)
