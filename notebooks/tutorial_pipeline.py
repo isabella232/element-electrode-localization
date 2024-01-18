5dec0510b21044ec8a92b84736f4189e00518c2d
MilagrosMarin <milmarina02@gmail.com>
2024-01-18 21:53:54 +0100

    revert previous commit

--- notebooks/tutorial_pipeline.py
+++ notebooks/tutorial_pipeline.py
@@ -22,33 +22,10 @@ dj.config["custom"]["database.prefix"] = os.getenv(
     "DATABASE_PREFIX", dj.config["custom"].get("database.prefix", "")
 )
 
-dj.config["custom"]["electrode_localization_root_data_dir"] = os.getenv(
-    "ELECTRODE_LOCALIZATION_ROOT_DATA_DIR",
-    dj.config["custom"].get("electrode_localization_root_data_dir", ""),
-)
-
 if "custom" not in dj.config:
     dj.config["custom"] = {}
 
 db_prefix = dj.config["custom"].get("database.prefix", "")
-
-
-# # Import the configured "ephys mode"
-# ephys_mode = os.getenv("EPHYS_MODE", dj.config["custom"].get("ephys_mode", "acute"))
-# if ephys_mode == "acute":
-#     from element_array_ephys import ephys_acute as ephys
-# elif ephys_mode == "chronic":
-#     from element_array_ephys import ephys_chronic as ephys
-# elif ephys_mode == "no-curation":
-#     from element_animal.export.nwb import subject_to_nwb
-#     from element_array_ephys import ephys_no_curation as ephys
-#     from element_array_ephys.export.nwb import ecephys_session_to_nwb, write_nwb
-#     from element_lab.export.nwb import element_lab_to_nwb_dict
-#     from element_session.export.nwb import session_to_nwb
-# elif ephys_mode == "precluster":
-#     from element_array_ephys import ephys_precluster as ephys
-# else:
-#     raise ValueError(f"Unknown ephys mode: {ephys_mode}")
 
 
 # Declare functions for retrieving data
@@ -69,14 +46,6 @@ def get_electrode_localization_dir():
         )
 
 
-@lab.schema
-class SkullReference(dj.Lookup):
-    definition = """
-    skull_reference   : varchar(60)
-    """
-    contents = zip(["Bregma", "Lambda"])
-
-
 # Activate schemas -------------
 lab.activate(db_prefix + "lab")
 subject.activate(db_prefix + "subject", linking_module=__name__)
@@ -84,11 +53,6 @@ session.activate(db_prefix + "session", linking_module=__name__)
 Session = session.Session
 Experimenter = lab.User
 
-# def get_session_directory(session_key):
-#     session_directory = (session.SessionDirectory & session_key).fetch1("session_dir")
-#     return pathlib.Path(session_directory)
-
-# Activate ephys and electrode-localization schema -----------------------------------
 ephys.activate(db_prefix + "ephys", db_prefix + "probe", linking_module=__name__)
 ProbeInsertion = ephys.ProbeInsertion
 electrode_localization.activate(
