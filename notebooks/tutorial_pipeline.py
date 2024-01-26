3b1b6760f1f3092c1797df9339bbe1759dd5bcc5
kushalbakshi <kushalbakshi@gmail.com>
2024-01-25 18:28:11 -0600

    Add - load_ccf_annotation

--- notebooks/tutorial_pipeline.py
+++ notebooks/tutorial_pipeline.py
@@ -3,6 +3,7 @@ import pathlib
 import datajoint as dj
 from element_animal import subject
 from element_animal.subject import Subject
+from element_interface.utils import find_full_path
 from element_array_ephys import probe, ephys_acute as ephys
 from element_lab import lab
 from element_lab.lab import Lab, Location, Project, Protocol, Source, User
@@ -20,10 +21,25 @@ dj.config["custom"]["database.prefix"] = os.getenv(
     "DATABASE_PREFIX", dj.config["custom"].get("database.prefix", "")
 )
 
-if "custom" not in dj.config:
-    dj.config["custom"] = {}
+dj.config["custom"]["ephys_root_data_dir"] = os.getenv(
+    "EPHYS_ROOT_DATA_DIR", dj.config["custom"].get("ephys_root_data_dir", "")
+)
 
 db_prefix = dj.config["custom"].get("database.prefix", "")
+
+
+# Declare functions for retrieving data
+def get_ephys_root_data_dir():
+    """Retrieve ephys root data directory."""
+    ephys_root_dirs = dj.config.get("custom", {}).get("ephys_root_data_dir", None)
+    if not ephys_root_dirs:
+        return None
+    elif isinstance(ephys_root_dirs, (str, pathlib.Path)):
+        return [ephys_root_dirs]
+    elif isinstance(ephys_root_dirs, list):
+        return ephys_root_dirs
+    else:
+        raise TypeError("`ephys_root_data_dir` must be a string, pathlib, or list")
 
 
 @lab.schema
@@ -48,3 +64,25 @@ ProbeInsertion = ephys.ProbeInsertion
 electrode_localization.activate(
     db_prefix + "electrode_localization", db_prefix + "ccf", linking_module=__name__
 )
+
+ccf_id = 0  # Atlas ID
+voxel_resolution = 100
+
+nrrd_filepath = find_full_path(
+    get_ephys_root_data_dir(), f"annotation_{voxel_resolution}.nrrd"
+)
+ontology_csv_filepath = find_full_path(get_ephys_root_data_dir(), "query.csv")
+
+
+if (
+    not (coordinate_framework.CCF & {"ccf_id": ccf_id})
+    and nrrd_filepath.exists()
+    and ontology_csv_filepath.exists()
+):
+    coordinate_framework.load_ccf_annotation(
+        ccf_id=ccf_id,
+        version_name="ccf_2017",
+        voxel_resolution=voxel_resolution,
+        nrrd_filepath=nrrd_filepath,
+        ontology_csv_filepath=ontology_csv_filepath,
+    )
